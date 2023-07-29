4e8eb75506b45654e9e2a94a1f2039200572a9b0
Sourav Choudhury <saurabc987@gmail.com>
2023-07-29 23:10:46 +0530

    Prepare For Production

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 //Cookie Parser Middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('App is running...');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -37,6 +33,20 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve(); //Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  // any route that is not api will be redirected to index.html
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('App is running...');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
