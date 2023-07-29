a93064f323f138eab542228426a80aac7f627699
Sourav Choudhury <saurabc987@gmail.com>
2023-07-29 21:21:30 +0530

    Paginate Products Route

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,4 +1,5 @@
 import { React } from 'react';
+import { useParams } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
@@ -6,7 +7,8 @@ import Message from '../components/Message';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
@@ -20,7 +22,7 @@ const HomeScreen = () => {
       ) : (
         <>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
