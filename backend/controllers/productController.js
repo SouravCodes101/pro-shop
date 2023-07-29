180d9b09ffb6a8fcdddd11ff4b6a61434c779598
Sourav Choudhury <saurabc987@gmail.com>
2023-07-29 15:48:08 +0530

    Edit Product - Admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,7 @@ import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
 
 //@desc Fetch All Products
-//GET /api/products
+//@route GET /api/products
 //@access Public
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
@@ -23,7 +23,7 @@ const getProductById = asyncHandler(async (req, res) => {
 });
 
 //@desc Create a Product
-//POST /api/products
+//@route POST /api/products
 //@access Private/Admin
 const createProduct = asyncHandler(async (req, res) => {
   const product = new Product({
@@ -42,4 +42,29 @@ const createProduct = asyncHandler(async (req, res) => {
   res.status(201).json(createProduct);
 });
 
-export { getProducts, getProductById, createProduct };
+//@desc Update a product
+//@route PUT /api/products/:id
+//@access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Resource not found');
+  }
+});
+
+export { getProducts, getProductById, createProduct, updateProduct };
